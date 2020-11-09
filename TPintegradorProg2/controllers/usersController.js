7690c1a8a1beb184dcb62f4ecf4eaeea90b4638d
mateopizzolo <mpizzolo@udesa.edu.ar>
2020-11-09 16:28:08 -0300

    desloguear funciona

--- TPintegradorProg2/controllers/usersController.js
+++ TPintegradorProg2/controllers/usersController.js
@@ -1,4 +1,7 @@
 let usersController = {
+    index: function(req, res) {
+        return res.render('logout')
+    },
     logout: function(req, res) {
         req.session.destroy();
         return res.redirect('/');
--- TPintegradorProg2/routes/users.js
+++ TPintegradorProg2/routes/users.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController')
 
-router.get('/', function(req, res) {
-  res.render('logout');
-});
-
+router.get('/', usersController.index);
 router.post('/logout', usersController.logout);
 
 module.exports = router;
